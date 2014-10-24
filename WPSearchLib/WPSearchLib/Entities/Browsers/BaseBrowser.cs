22591777d2b1da669b8fdbad02df8065456038a9
Toon Hooyberghs <hooyberghs.toon@gmail.com>
2014-10-24 20:41:51 +0200

    Add Browser Logic

---
+++ WPSearchLib/WPSearchLib/Entities/Browsers/BaseBrowser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WPSearchLib.Entities.Browsers
+{
+    public abstract  class BaseBrowser
+    {
+        public abstract string Search(string url);
+
+        public virtual HtmlDocument SearchItem(string url)
+        {
+            var result = Search(url);
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(result);
+            return doc;
+        }
+    }
+}
---
+++ WPSearchLib/WPSearchLib/Entities/Browsers/PhantomJS/PhantomBrowser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.PhantomJS;
+
+namespace WPSearchLib.Entities.Browsers.PhantomJS
+{
+    public class PhantomBrowser : BaseBrowser
+    {
+        public override string Search(string url)
+        {
+            const string PhantomDirectory = @"..\PhantomJS";
+            var driverService = PhantomJSDriverService.CreateDefaultService(PhantomDirectory);
+            driverService.HideCommandPromptWindow = false;
+            driverService.LoadImages = false;
+            string pageSource = null;
+
+            using (var phantomDriver = new PhantomJSDriver(driverService))
+            {
+                phantomDriver.Url = url;
+                pageSource = phantomDriver.PageSource;
+            }
+
+            return pageSource;
+
+        }
+    }
+}
