6e9703b1ea30c070acc311a16bd553733540a78a
Toon Hooyberghs <hooyberghs.toon@gmail.com>
2014-10-25 21:56:02 +0200

    Add SimpleHtmlBrowser

---
+++ WPSearchLib/WPSearchLib/Entities/Browsers/SimpleHtmlBrowser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Fizzler.Systems.HtmlAgilityPack;
+
+namespace WPSearchLib.Entities.Browsers
+{
+    public class SimpleHtmlBrowser :BaseBrowser 
+    {
+        HtmlWeb WebAccess { get; set; }
+
+        public SimpleHtmlBrowser()
+        {
+          WebAccess = new HtmlWeb();
+        }
+
+        protected override string Search(string url)
+        {
+            var pageSource = WebAccess.Load(url);
+            return pageSource.ToString();
+        }
+
+        protected async override Task<string> SearchAsync(string url)
+        {
+            var task = Task.Run(() => Search(url));
+            return await task; 
+        }
+        
+    }
+}
